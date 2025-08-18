3280d428f486e4ba06c89edfb7ca68d64c3a3da1
justbeanpole <dbtjd7282@gmail.com>
2025-08-19 05:25:43 +0900

    chore:scrollbar-hide 플러그인 설치

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,9 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        // eslint-disable-next-line no-undef
+        require("tailwind-scrollbar-hide")
+    ],
 }
 
