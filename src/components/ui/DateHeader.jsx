7073ee6c05548ea12d8dc44b1761986fed4df10e
justbeanpole <dbtjd7282@gmail.com>
2025-08-19 05:22:25 +0900

    feat:메인화면 날짜데이터 입력

--- src/components/ui/DateHeader.jsx
+++ src/components/ui/DateHeader.jsx
@@ -1,7 +1,9 @@
 const DateHeader = () => {
+    const today = new Date();
+    const day = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
     return (
-        <header className="mb-8">
-            <h1 className="text-3xl font-medium p-5 text-gray-800">{"오늘은 8월 4일 월요일입니다"}</h1>
+        <header className="mb-4 text-center">
+            <h1 className="text-3xl font-medium p-5 text-gray-800">{`오늘은 ${today.getMonth()+1}월 ${today.getDate()}일 ${day[today.getDay()-1]}입니다`}</h1>
         </header>
     )
 }
