6b117d69cca69d350e60f57c03d66d0b62807afd
justbeanpole <dbtjd7282@gmail.com>
2025-09-01 16:21:57 +0900

    style:스타일 수정

--- src/components/ui/TimeTable.jsx
+++ src/components/ui/TimeTable.jsx
@@ -4,13 +4,13 @@ import TodoItem from "../todo/TodoItem.jsx";
 
 const TimeTable = ({hours, todos, deleteTodo, updateTodo, handleChangeTitle,handleToggleDone}) => {
     return (
-        <div className="basis-3/5 p-5 flex flex-col h-screen bg-white text-gray-800 rounded-2xl">
+        <div className="w-3/4 m-auto p-5 flex flex-col h-screen bg-white text-gray-800 rounded-2xl">
             <h1 className="text-black font-bold text-2xl pb-4">Time Table</h1>
-            <div className="grid grid-cols-[0.2fr_1fr_1fr] grid-rows-19 text-center h-full overflow-y-scroll scrollbar-hide">
+            <div className="grid grid-cols-[0.2fr_1fr_1fr] grid-rows-19 h-full overflow-y-scroll scrollbar-hide">
                 {/* Header */}
                 <div className="text-sm font-semibold text-gray-500 border-b border-r border-gray-300 py-2"></div>
-                <div className="text-sm font-semibold text-gray-500 border-b border-r border-gray-300 py-2">:00</div>
-                <div className="text-sm font-semibold text-gray-500 border-b border-gray-300 py-2">:30</div>
+                <div className="text-sm font-semibold text-center text-gray-500 border-b border-r border-gray-300 py-2">:00</div>
+                <div className="text-sm font-semibold text-center text-gray-500 border-b border-gray-300 py-2">:30</div>
 
                 {/* Content */}
                 {hours.map(([t00, t30], idx) => (
@@ -21,7 +21,7 @@ const TimeTable = ({hours, todos, deleteTodo, updateTodo, handleChangeTitle,hand
                         </div>
                         <Droppable droppableId={`slot-${t00}`}>
                             {(provided) => (
-                                <div className="border-b border-r border-gray-300 hover:bg-gray-50 relative"
+                                <div className="border-b border-r min-w-0 border-gray-300 hover:bg-gray-50 relative"
                                      ref={provided.innerRef}
                                      {...provided.droppableProps}
                                 >
@@ -52,14 +52,13 @@ const TimeTable = ({hours, todos, deleteTodo, updateTodo, handleChangeTitle,hand
                                                 </Draggable>)
                                         })
                                     }
-                                    <div className="absolute"
-                                    >{provided.placeholder}</div>
+                                    {provided.placeholder}
                                 </div>
                             )}
                         </Droppable>
                         <Droppable droppableId={`slot-${t30}`}>
                             {(provided) => (
-                                <div className="border-b border-gray-300 hover:bg-gray-50"
+                                <div className="border-b min-w-0 border-gray-300 hover:bg-gray-50"
                                      ref={provided.innerRef}
                                      {...provided.droppableProps}
                                 >
